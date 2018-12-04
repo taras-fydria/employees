252320a698c19d1bca88a256fbfec09af5600946
taras-fydria <taras.fydria@gmail.com>
2018-12-04 23:46:06 +0200

    first commit

---
+++ js/widget.js
@@ -0,0 +1,39 @@
+
+    console.log('hbkjuhfv');
+    
+
+
+
+
+
+
+
+
+
+// ;
+// (function () {
+//     const unorderList = document.querySelector('ba-list'),
+//         liTMPL = document.querySelector('#listTMPL').innerHTML;
+
+// console.log(liTMPL);
+// console.log(unorderList);
+
+
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', "../data/employees.json");
+//     xhr.send();
+
+//     xhr.onload = function () {
+//         let ajax = this.response;
+//         console.log(ajax);
+//         let response = JSON.parse(ajax.response);
+//         console.log(response);
+
+
+
+//     }
+
+
+
+
+// })();
